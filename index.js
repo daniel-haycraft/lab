eefe014f134adc7b4c63bd7f9fc0c6bb4c231e57
Daniel Haycraft <111464698+daniel-haycraft@users.noreply.github.com>
2022-08-24 00:59:55 +0000

    added ques. 11

--- index.js
+++ index.js
@@ -1,74 +1,75 @@
-// // let lovesCode = true
 
-// // if (lovesCode === true){
-// //   console.log('I love to code!')
-// // } else {
-// //   console.log("coding has its challenges")
-// // }
-// // // 
+let lovesCode = true
+
+if (lovesCode === true){
+  console.log('I love to code!')
+} else {
+  console.log("coding has its challenges")
+}
+// 
 
 
-// let amysAge = 29;
-// let brittanisAge = 35;
-// let amysBirthYear = 1991;
-// let brittanisBirthYear = 1986;
+let amysAge = 29;
+let brittanisAge = 35;
+let amysBirthYear = 1991;
+let brittanisBirthYear = 1986;
 
 // ////////// PROBLEM 2 //////////
-// // if (amysAge > brittanisAge){
-// //   console.log('Amy is older')
-// // } else if (amysAge < brittanisAge){
-// //   console.log('Brittanis is older')
-// // } else {
-// //   console.log('brittani and amy are the samie')
-// // }
+if (amysAge > brittanisAge){
+  console.log('Amy is older')
+} else if (amysAge < brittanisAge){
+  console.log('Brittanis is older')
+} else {
+  console.log('brittani and amy are the samie')
+}
 
-// // if (amysBirthYear > brittanisBirthYear){
-// //   console.log('Amy and Britt were not born in the same year')
-// // } else {
-// //   console.log('Amy and Britt were born in the same year')
-// // }
+if (amysBirthYear > brittanisBirthYear){
+  console.log('Amy and Britt were not born in the same year')
+} else {
+  console.log('Amy and Britt were born in the same year')
+}
 
 // ////////// PROBLEM 4 //////////
 
-// let temp = 70;
-// let rain = true;
-// let snow = true
+let temp = 70;
+let rain = true;
+let snow = true
 
 
 
 //   // Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
 //   // If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
 
-// if (temp >= 80 && rain === true){
-//   console.log("you should wear a t-shirt and take an umbrella with you today")
-// } else if (temp >= 60 && temp <= 80 && rain === true){
-//   console.log(`where a rain jacket its quite cold outside`)
-// } else if (temp > 60 && rain === true){
-//   console.log(`the temp is ${temp} so gear up. you'll need both a jacket and an umbrella`)
-// } else if (temp > 35 && snow === true){
-//   console.log('its gonna be a cold day people, the chances for snow is around 100% today! Grab your snow jacket, boots, gloves, and most importantly skis or a snowboard!')
-// } else {
-//   console.log('i live in arizona dont take my advice')
-// }
+if (temp >= 80 && rain === true){
+  console.log("you should wear a t-shirt and take an umbrella with you today")
+} else if (temp >= 60 && temp <= 80 && rain === true){
+  console.log(`where a rain jacket its quite cold outside`)
+} else if (temp > 60 && rain === true){
+  console.log(`the temp is ${temp} so gear up. you'll need both a jacket and an umbrella`)
+} else if (temp > 35 && snow === true){
+  console.log('its gonna be a cold day people, the chances for snow is around 100% today! Grab your snow jacket, boots, gloves, and most importantly skis or a snowboard!')
+} else {
+  console.log('i live in arizona dont take my advice')
+}
 
 
 
-// for (let i = 0; i < 10 && i < 10; i++) {
-//   console.log("hello there") 
-// }
+for (let i = 0; i < 10 && i < 10; i++) {
+  console.log("hello there") 
+}
 
 // Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
 
-// for (let i = 0; i <= 10; i++) {
-//   console.log(i)
-// }
+for (let i = 0; i <= 10; i++) {
+  console.log(i)
+}
 
-// for (let i = 10; i >= 0; i--) {
-//   console.log(i)
-// }
+for (let i = 10; i >= 0; i--) {
+  console.log(i)
+}
 
-// let score = 0;
-// let passingScore = 7;
+let score = 0;
+let passingScore = 7;
 
 
 // //   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
@@ -102,7 +103,7 @@ console.log(changeMyMind)
 
 //CODE HERE
 
-if (changeMyMind != true){
+if (changeMyMind !== true){
   changeMyMind = true
   console.log(changeMyMind)
 }
@@ -111,7 +112,15 @@ if (changeMyMind != true){
 
 ////////// PROBLEM 11 //////////
 
-let z = 5;
+
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
-//CODE HERE
+z = 5
+ while (z > 0){
+ for (let i = z; i > 0; i--){
+   console.log(i)
+ }
+  z--
+ }
+
+
